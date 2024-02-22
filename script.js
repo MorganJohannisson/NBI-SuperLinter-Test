06ba6e3e9b3c4f9207c928c44e89360741bd8d0c
MorganJohannisson <149048872+MorganJohannisson@users.noreply.github.com>
2024-02-22 10:02:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const x = 5
 const y = 7
+
 function double (x) { return x * 2 )
-console.print('Dubbelt av 5 och 7 är ' + (double(x) + double(y)))
+
+                     console.print('Dubbelt av 5 och 7 är ' + (double(x) + double(y)))
